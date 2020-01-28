b5a20ed3d93495e49ffbda405e928c45f6b1eeca
Artem <freez2018@narod.ru>
2020-01-27 20:25:35 -0600

    vog_backend_codechalleneg initial commit
     vog_backend_codechalleneg initial commit

---
+++ Artem_Mikhailov_vog_backend_codechalleneg/VogCodeChallenge.API/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Address
+    {
+        public string Id { get; set; }
+        public string AddressLine_1 { get; set; }
+        public AddressType Type { get; set; }
+    }
+
+    public enum AddressType
+    {
+        Mailing,
+        Billing
+    }
+
+}
---
+++ Artem_Mikhailov_vog_backend_codechalleneg/VogCodeChallenge.API/Services/DepartmentService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Models;
+
+namespace VogCodeChallenge.API.Services
+{
+    public interface IDepartmentService
+    {
+      IEnumerable<Employee> GetAll();
+      IList<Employee> ListAll();
+    }
+
+    public class DepartmentService : IDepartmentService
+    {
+        public IEnumerable<Employee> GetAll()
+        {
+            return null;
+        }
+        public IList<Employee> ListAll()
+        {
+            return null;
+        }
+    }
+}
